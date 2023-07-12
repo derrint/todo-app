b2151caec91629baa8970892e601278991306750
Derrint <lorensioderrint@gmail.com>
2023-07-12 21:17:29 +0700

    update lint config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-        <Component {...pageProps} />
+    <Component {...pageProps} />
     </Provider>
   );
 }
