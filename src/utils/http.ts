6ea3e05bf093c78f97c2a43392ee448b41128e13
Derrint <lorensioderrint@gmail.com>
2024-03-17 19:16:40 +0700

    adjust host & port

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://dummyjson.com',
+  baseURL: 'http://localhost:3000/api',
   headers: {
     'Content-type': 'application/json'
   }
