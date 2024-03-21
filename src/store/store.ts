eb1cbb75e3594995ece86dbc6a0d0a79f6f5c080
Derrint <lorensioderrint@gmail.com>
2024-03-21 08:58:12 +0700

    adjust store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,22 @@
-import { setupListeners } from '@reduxjs/toolkit/query'
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 import todoApi from '@/api/todo'
 import authApi from '@/api/auth'
 
-export const store = configureStore({
-  reducer: {
-    [todoApi.reducerPath]: todoApi.reducer,
-    [authApi.reducerPath]: authApi.reducer
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todoApi.middleware).concat(authApi.middleware)
-})
+export const setupStore = (preloadedState?: any) => {
+  return configureStore({
+    reducer: {
+      [todoApi.reducerPath]: todoApi.reducer,
+      [authApi.reducerPath]: authApi.reducer
+    },
+    preloadedState,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todoApi.middleware).concat(authApi.middleware)
+  })
+}
+
+const store = setupStore()
+
 setupListeners(store.dispatch)
 
 export default store
